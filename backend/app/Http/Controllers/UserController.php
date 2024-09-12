380bb70d2294e17d30de90da32c11d25182e0039
Hoang Bao Truc <hoangbaotruc@Hoangs-MacBook-Pro.local>
2024-09-12 18:48:32 +0700

    setup backend

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\User;
+use App\Http\Resources\UserResource;
+
+
+
+class UserController extends Controller
+{
+    protected $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // $users = $this->user->pagninate(5);
+        // $users = $this->user;
+        $users = User::all();
+        $usersResource = UserResource::collection($users);
+
+        return response()->json([
+            'data' => $usersResource,
+        ],Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -17,9 +17,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'id',
         'name',
         'email',
         'password',
+        'phone',
+        'status',
     ];
 
     /**
